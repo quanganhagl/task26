87d40b867a51020cbb7d526e1d8e6980b4d7134b
AGL_Hà Quang Anh (アイン) <ha_quang_anh@allgrow-labo.jp>
2023-01-06 17:46:14 +0700

    update all

---
+++ src/_public/assets/js/main.js
@@ -0,0 +1,9 @@
+$(".c-designs__slickimgscontainer").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: $(".c-designs__slickleftbtn"),
+  nextArrow: $(".c-designs__slickrightbtn"),
+});
